0d74a435563d052d995c7782301df9a35385b7d3
Ramana Ambore <ramana.ambore.gmail.com>
2025-01-03 22:43:02 +0530

    directory structure cleaned up, yaml for data and config added, css is added in .css file

--- src/utils/constants.py
+++
@@ -1,9 +0,0 @@
-about_me_summary = """
-    FinTech professional with extensive expertise in legacy system re-engineering and modernization. 
-    Well-versed in trading platforms and high-volume data transmission systems. Expertise in distributed/cloud technologies, 
-    mainframe systems, data analytics, and agile methodologies to drive business adaptability. My first mainframe user 
-    ID and nickname, 'Rambo,' reflects my tenacity.
-"""
-
-about_me_name = "RAMANA RAO AMBORE (RAMBO)"
-about_me_designation = "APPLICATION DEVELOPMENT DIRECTOR"
