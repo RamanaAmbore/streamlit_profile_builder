657c915250a4112374caef8755122f5eb0dabb93
Ramana Ambore <ramana.ambore.gmail.com>
2025-01-05 21:38:58 +0530

    hyper link code cleanup and restructured

--- app.py
+++ app.py
@@ -20,16 +20,33 @@ def container(*args, **kwargs):
     return function(*args, **kwargs)
 
 
-def disp_icon_text(icon_link, text):
-    st.markdown(
-        f"""
-        <div style='display: flex; align-items: center; font-weight: bold; font-size: 20; padding:0rem;'>
-            <img src='{config[icon_link]}' style='width: 30px; margin-right: 10px;'>
-            <span> {text} </span>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+def disp_icon_text(parm_text, link_flag=True):
+    icon = config[f'{parm_text}_icon']
+    text = config[f'{parm_text}_text']
+
+
+    if link_flag:
+        link = config[f'{parm_text}_link']
+
+        st.markdown(
+            f"""
+            <div style='display: flex; align-items: center; font-weight: bold; font-size: 20; padding:0rem;'>
+                <img src='{icon}' style='width: 30px; margin-right: 10px;'>
+                <span> <a href="{link}">{text}</a></span>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+    else:
+        st.markdown(
+            f"""
+            <div style='display: flex; align-items: center; font-weight: bold; font-size: 20; padding:0rem;'>
+                <img src='{icon}' style='width: 30px; margin-right: 10px;'>
+                <span> {text} </span>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
 
 if 'logger' not in st.session_state:
@@ -58,39 +75,22 @@ if __name__ == '__main__':
         st.write(f'#### {prof['designation']}')
         st.write(prof['summary'])
 
-    loc_col, _, mail_col, _, phone_col = st.columns([0.7, .05, 1, .05, 1], vertical_alignment='center')
+    loc_col, _, mail_col, _, phone_col = st.columns([0.52, .05, 0.5, .05, 0.5], vertical_alignment='center')
     with loc_col:
-        disp_icon_text('loc_icon', 'Des Moines, Iowa')
+        disp_icon_text('loc')
     with mail_col:
-        disp_icon_text('phone_icon', 'Ramana.Ambore@gmail.com')
+        disp_icon_text('mail')
     with phone_col:
-        disp_icon_text('mail_icon', '(XXX) XXX-XXXX')
+        disp_icon_text('phone', False)
 
-    linkedin_col, _, github_col = st.columns([1.5, .05, 2], vertical_alignment='center')
+    linkedin_col, _, github_col = st.columns([1, .05, 2], vertical_alignment='center')
     with linkedin_col:
-        disp_icon_text('linkedin_icon', config['linkedin'])
+        disp_icon_text('linkedin')
     with github_col:
-        disp_icon_text('github_icon', config['github'])
+        disp_icon_text('github')
 
-    medium_col, _, streamlit_col = st.columns([1.5, .05, 2], vertical_alignment='center')
+    medium_col, _, streamlit_col = st.columns([1, .05, 2], vertical_alignment='center')
     with medium_col:
-        disp_icon_text('medium_icon', config['medium'])
+        disp_icon_text('medium')
     with streamlit_col:
-        disp_icon_text('streamlit_icon', config['streamlit'])
-        # linkedin_icon1_, github_icon2, _, github_link2, _, medium_icon3, _, medium_link3 = st.columns(
-        #     [.2, 1, .005, 3,
-        #      .2, 1, .005, 5,
-        #      .2, 1, .005, 5
-        #      ])
-        # with linkedin_icon1:
-        #     st.image(config['linkedin_icon'], width=50, clamp=True)
-        # with linkedin_link1:
-        #     container(st.write)('phone_icon', "##### test")
-        # with github_icon2:
-        #     st.image(config['github_icon'], width=50, clamp=True)
-        # with github_link2:
-        #     container(st.write)('contact_icon', "##### test")
-        # with medium_icon3:
-        #     st.image(config['medium_icon'], width=50, clamp=True)
-        # with medium_link3:
-        #     container(st.write)('contact_icon', "##### test")
+        disp_icon_text('streamlit')
